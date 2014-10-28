490c45ddf0bb9d139a492cf22f554e9a06154a98
Walter Tude <walter_tude@hotmail.com>
2014-10-28 14:52:06 -0200

    first commit

---
+++ Quiron.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Quiron.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTetQuiron
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
